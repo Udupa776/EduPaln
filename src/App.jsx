542046c69451f8995c008f68390c6f11e0a719cb
Udupa776 <udupayashwant@gmail.com>
2025-09-28 16:42:43 +0530

    app .jsx change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
+import viteLogo from './assets/vite.svg'
 import Page from './page.jsx'
 import './App.css'
 
